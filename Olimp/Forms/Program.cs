f3a45ef128a3fb691e166dcf94ac44ff79d7e478
DiZay <74287343+ZDD-sup@users.noreply.github.com>
2023-02-19 17:35:22 +0600

    Добавьте файлы проекта.

---
+++ Olimp/Forms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Forms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Olimp/Kybik_rubik/Kybik_rubik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kybik_rubik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
