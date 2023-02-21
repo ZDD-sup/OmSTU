d19b410ea0aa84e64770177bb8ea0148ed8f7343
DiZay <74287343+ZDD-sup@users.noreply.github.com>
2023-02-22 00:26:45 +0600

    Создал класс Person

--- LB-CL/_16_02/Program.cs
+++ LB-CL/_16_02/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+        class Persn
+        {
+            //private string Name;//ФИО
+            //private string Date;//дата рождения
+            //private string Education;//где образование
+            //private string Item;//предмет
+            public List<string> people = new List<string>();
+            private List<string> history = new List<string>();
+            public void Input()
+            {
+                Console.Write("Введите ФИО учителя: ");
+                people.Add(Convert.ToString(Console.ReadLine()));
+                Console.Write("Укажите дату рождения учителя: ");
+                people.Add(Convert.ToString(Console.ReadLine()));
+                Console.Write("Укажите место обучнеия: ");
+                people.Add(Convert.ToString(Console.ReadLine()));
+                Console.Write("Укажите предмет преподавания учителем: ");
+                people.Add(Convert.ToString(Console.ReadLine()));
+                Console.WriteLine("Заполните трудовую книжку.(Enter - записать новый стаж, Space - закончить запись)");
+                while(Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    Employment_history();
+                }
+                people.AddRange(history);
+            }
+            private void Employment_history()
+            {
+                Console.Write("Начало работы: ");
+                history.Add(Convert.ToString(Console.ReadLine()));
+                Console.Write("Увольнение: ");
+                history.Add(Convert.ToString(Console.ReadLine()));
+                Console.Write("Наименование организации: ");
+                history.Add(Convert.ToString(Console.ReadLine()));
+            }
         }
     }
 }
+/*
+Неоюходимо создать базу данных состоящую из объектов следующих структур:
+ - объект описывает учителя в школе и содержит слуд. поля:
+ - - фио
+ - - дата рождения
+ - - наименование организации где получил образование
+ - - предмет
+ - - ТРУДОВАЯ КНИЖКА
+ - - - год поступление в орг
+ - - - наименования организации
+ - - - дата увольнение
+Поиск учителей из базы даных
+ - по предмету
+ - по стажу работы
+ - общий стаж работы
+ - вывод всех сотрудников
+{(1),(2),(3)}
+ */
