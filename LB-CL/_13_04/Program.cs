dd181662c022ce3a73dcb0d079ab2ad67884a26e
DiZay <74287343+ZDD-sup@users.noreply.github.com>
2023-04-16 15:53:19 +0600

    Проверить

--- LB-CL/_13_04/Program.cs
+++ LB-CL/_13_04/Program.cs
@@ -5,9 +5,9 @@
         static void Main()
         {
             double x, y;
-            IMath IMath= new IMath();
-            Print();
-            void Print()
+
+            Print_start();
+            void Print_start()
             {
                 Console.Clear();
                 x = Convert.ToDouble(Console.ReadLine());
@@ -16,59 +16,58 @@
                     + "\n2. Разница"
                     + "\n3. Умножение"
                     + "\n4. Деление");
-                switch (Console.ReadKey().Key)
+                void Print()
                 {
-                    case ConsoleKey.D1://сумма
-                        Console.WriteLine(IMath.sum(x, y));
-                        Console.ReadKey();
-                        Print();
-                        break;
-                    case ConsoleKey.D2://разница
-                        Console.WriteLine(IMath.diff(x, y));
-                        Console.ReadKey();
-                        Print();
-                        break;
-                    case ConsoleKey.D3://умножение
-                        Console.WriteLine(IMath.mult(x, y));
-                        Console.ReadKey();
-                        Print();
-                        break;
-                    case ConsoleKey.D4://деление
-                        if (y != 0)
-                            Console.WriteLine(IMath.div(x, y));
-                        else
-                            Console.WriteLine("На ноль делить нельзя!");
-                        Console.ReadKey();
-                        Print();
-                        break;
-                    case ConsoleKey.D5://степень
-                        Console.WriteLine("В разработке, ждите обновление");
-                        Console.ReadKey();
-                        Print();
-                        break;
-                    case ConsoleKey.D6:
-                        break;
-                    default:
-                        Print();
-                        break;
-                }
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.D1:
+                            break;
+                        case ConsoleKey.D2:
+                            break;
+                        case ConsoleKey.D3:
+                            break;
+                        case ConsoleKey.D4:
+                            break;
+                        default:
+                            Print();
+                            break;
+                    }
 
+                }
             }
         }
     }
-    interface IMath
+    class Math : IMath
     {
-        static double Sum(double x, double y) => x + y;
-        static double Mult(double x, double y) => x * y;
-        static double Diff(double x, double y) => x - y;
-        static double Div(double x, double y) => x / y;
-        
         public delegate double Deleg(double x, double y);
+
+        public void Sum(double x, double y)
+        { 
+            Console.WriteLine(x + y);
+        }
+        public void Mult(double x, double y)
+        {
+            Console.WriteLine(x * y);
+        }
+        public void Diff(double x, double y)
+        {
+            Console.WriteLine(x - y);
+        }
+        public void Div(double x, double y)
+        {
+            Console.WriteLine(x / y);
+        }
 
         Deleg sum = Sum;
         Deleg mult = Mult;
         Deleg diff = Diff;
         Deleg div = Div;
-        
+    }
+    interface IMath
+    {
+        static double Sum(double x, double y);
+        static double Mult(double x, double y);
+        static double Diff(double x, double y);
+        static double Div(double x, double y);
     }
 }
