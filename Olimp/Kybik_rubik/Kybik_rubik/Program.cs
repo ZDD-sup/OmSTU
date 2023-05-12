a84fe08fd6217fc04c1f9d6c67c1cb4f8269f39d
DiZay <74287343+ZDD-sup@users.noreply.github.com>
2023-05-12 09:49:08 +0600

    Проверить

--- Olimp/Kybik_rubik/Kybik_rubik/Program.cs
+++ Olimp/Kybik_rubik/Kybik_rubik/Program.cs
@@ -1,21 +1,95 @@
-﻿/*
- Создать трехмерный массив, представляющий кубик Рубика, заполненный числами от 1 до N^3.
-
-Считать начальные координаты элемента кубика (XN, YN, ZN) из входного файла.
-
-Пройти по каждой строке входного файла, выполняя соответствующее вращение блоков кубика.
-
-Для каждого вращения блоков кубика, вычислить индексы блоков, которые будут затронуты вращением, и выполнить само вращение.
-
-Найти новые координаты элемента кубика после всех вращений и вывести их в выходной файл.
- */
-namespace Kybik_rubik
+﻿namespace Kybik_rubik
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "D:/OmSTU/Olimp/Kybik_rubik/Test/input_s1_01.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line = "";
+                line = reader.ReadLine();
+                string[] s = line.Split(' ');
+                int n = Convert.ToInt32(s[0]);
+                int m = Convert.ToInt32(s[1]);
+
+                line = reader.ReadLine();
+                string[] pos = line.Split(' ');
+                int tmp = 0;
+                int xPos = Convert.ToInt32(pos[0]);
+                int yPos = Convert.ToInt32(pos[1]);
+                int zPos = Convert.ToInt32(pos[2]);
+
+                for (int i = 0; i < m; i++)
+                {
+                    line = reader.ReadLine();
+                    string[] rot = line.Split(' ');
+                    string axis = rot[0];
+                    int cord = Convert.ToInt32(rot[1]);
+                    int dir = Convert.ToInt32(rot[2]);
+
+                    if (axis == "X")
+                    {
+                        if (xPos == cord)
+                        {
+                            if (dir > 0)
+                            {
+                                tmp = zPos;
+                                zPos = n + 1 - yPos;
+                                yPos = tmp;
+                            }
+                            else
+                            {
+                                tmp = yPos;
+                                yPos = n + 1 - zPos;
+                                zPos = tmp;
+                            }
+
+                        }
+                    }
+                    if (axis == "Y")
+                    {
+                        if (yPos == cord)
+                        {
+                            if (dir > 0)
+                            {
+                                tmp = zPos;
+                                zPos = n + 1 - xPos;
+                                xPos = tmp;
+                            }
+                            else
+                            {
+                                tmp = xPos;
+                                xPos = n + 1 - zPos;
+                                zPos = tmp;
+                            }
+                        }
+                    }
+                    if (axis == "Z")
+                    {
+                        if (zPos == cord)
+                        {
+                            if (dir > 0)
+                            {
+                                tmp = yPos;
+                                yPos = n + 1 - xPos;
+                                xPos = tmp;
+                            }
+                            else
+                            {
+                                tmp = xPos;
+                                xPos = n + 1 - yPos;
+                                yPos = tmp;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"{xPos};{yPos};{zPos}");
+                using (StreamWriter writer = new StreamWriter("C:/Users/djego/OneDrive/Рабочий стол/csharp/lab02.02/output.txt", true))
+                {
+                    writer.Write($"{xPos};{yPos};{zPos}");
+                }
+            }
         }
     }
 }
